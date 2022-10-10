990dea6008936e6cf06a10c7a28bc024630862d5
owdji <omar.gonin@gmail.com>
2022-10-10 17:21:13 +0200

    ajout requête UPDATE

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/M/comem-archidep-php-todo-exercise/');
+define('BASE_URL', '/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
@@ -44,7 +44,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = ''; // IMPLEMENT ME
+        $updateQuery = 'UPDATE todo SET done = NOT done WHERE id =' . $id . ';'; // IMPLEMENT ME
         if(!$db->query($updateQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = 'SELECT * FROM todo; '; // IMPLEMENT ME
+$selectQuery = 'SELECT * FROM todo ORDER BY title ASC;'; // IMPLEMENT ME
 $items = $db->query($selectQuery);
 ?>
 
